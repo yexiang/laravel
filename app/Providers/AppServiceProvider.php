1fc228003621ba1742ebfe6715b1423973a91245
Your Name <you@example.com>
2022-11-21 01:47:57 +0800

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-      if (env('APP_ENV') !== 'local') {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-      }
+      //if (env('APP_ENV') !== 'local') {
+        //\Illuminate\Support\Facades\URL::forceScheme('https');
+      //}
     }
 }
