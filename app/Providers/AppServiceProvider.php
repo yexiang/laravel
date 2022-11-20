8680b75fe3c8b6cccd8a5e1a8be7b06b620dd54f
Your Name <you@example.com>
2022-11-21 01:35:45 +0800

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
-          \URL::forceScheme('https');
-        }
 
+      if (env('APP_ENV') === 'production') {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+      }
     }
 }
